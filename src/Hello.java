db3e855807f82dfbfcd4f06925b5fcadf95c166e
liuhongliang <liuhongliang@BIH-D-2412.youdao.internal>
2014-08-13 09:56:16 +0800

    Hello youdao!

--- src/Hello.java
+++ src/Hello.java
@@ -1,12 +1,9 @@
-import java.io.IOException;
-import java.io.StreamTokenizer;
-import java.io.StringReader;
 
 public class Hello {
 
-	@SuppressWarnings("static-access")
-	public static void main(String[] args) {
-		System.out.println("Hello");
-		
-	}
+    @SuppressWarnings("static-access")
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("hello youdao!");
+    }
 }
