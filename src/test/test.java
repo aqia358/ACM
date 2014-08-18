3d63d6321e14abe8b7ace38fbaf0b20c1aefb73b
liuhongliang <liuhongliang@BIH-D-2412.youdao.internal>
2014-08-18 19:35:06 +0800

    test

--- src/test/test.java
+++ src/test/test.java
@@ -10,65 +10,69 @@ import java.util.Set;
 
 public class test {
 
-	public static List<List<Integer>> combine(int n, int k){
-		List<List<Integer>> re = new ArrayList<List<Integer>>();
-		combine(n, k, 1, re, new ArrayList<Integer>());
-		return re;
-	}
-	
-	public static void combine(int n, int num, int start, List<List<Integer>> re, ArrayList<Integer> l){
-		if(num == 0){
-			re.add(l);
-			return;
-		}
-		for(int i = start; i <= n; i++){
-			ArrayList<Integer> c = (ArrayList<Integer>) l.clone();
-			c.add(i);
-			combine(n, num - 1, i + 1, re, c);
-		}
-	}
-	
-	public static int findLadders(String start, String end, Set<String> dict){
-		Queue<String> q = new LinkedList<String>();
-		Map<String, Integer> map = new HashMap<String, Integer>();
-		q.add(start);
-		map.put(start, 1);
-		while(!q.isEmpty()) {
-			String temp = q.poll();
-			int step = map.get(temp);
-			StringBuilder sb = new StringBuilder(temp);
-			for(int i = 0; i < sb.length(); i++){
-				char c = sb.charAt(i);
-				for(char j = 'a'; j <= 'z'; j++){
-					if(sb.charAt(i) != j){
-						sb.setCharAt(i, j);
-						String t = sb.toString();
-						if(t.equals(end)){
-							return step + 1;
-						}
-						if(dict.contains(t) && !q.contains(t)){
-							q.add(t);
-							map.put(t, step + 1);
-						}
-						sb.setCharAt(i, c);
-					}
-				}
-			}
-		
-		}
-		return 0;
-	}
-	
-	public static void ipConver(String ip) {
-		String[] strs = ip.split("\\.");
-		for(String t : strs){
-			System.out.println(Integer.parseInt(t));
-		}
-	}
-	
-	public static void main(String[] args) {
-		combine(4, 2);
-		ipConver("192.168.2.22");
-	}
+    public static List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> re = new ArrayList<List<Integer>>();
+        combine(n, k, 1, re, new ArrayList<Integer>());
+        return re;
+    }
+
+    public static void combine(int n, int num, int start,
+            List<List<Integer>> re, ArrayList<Integer> l) {
+        if (num == 0) {
+            re.add(l);
+            return;
+        }
+        for (int i = start; i <= n; i++) {
+            ArrayList<Integer> c = (ArrayList<Integer>) l.clone();
+            c.add(i);
+            combine(n, num - 1, i + 1, re, c);
+        }
+    }
+
+    public static int findLadders(String start, String end, Set<String> dict) {
+        Queue<String> q = new LinkedList<String>();
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        q.add(start);
+        map.put(start, 1);
+        while (!q.isEmpty()) {
+            String temp = q.poll();
+            int step = map.get(temp);
+            StringBuilder sb = new StringBuilder(temp);
+            for (int i = 0; i < sb.length(); i++) {
+                char c = sb.charAt(i);
+                for (char j = 'a'; j <= 'z'; j++) {
+                    if (sb.charAt(i) != j) {
+                        sb.setCharAt(i, j);
+                        String t = sb.toString();
+                        if (t.equals(end)) {
+                            return step + 1;
+                        }
+                        if (dict.contains(t) && !q.contains(t)) {
+                            q.add(t);
+                            map.put(t, step + 1);
+                        }
+                        sb.setCharAt(i, c);
+                    }
+                }
+            }
+
+        }
+        return 0;
+    }
+
+    public static void ipConver(String ip) {
+        String[] strs = ip.split("\\.");
+        for (String t: strs) {
+            System.out.println(Integer.parseInt(t));
+        }
+    }
+
+    public static void main(String[] args) {
+//        combine(4, 2);
+//        ipConver("192.168.2.22");
+        List<String> list = new ArrayList<String>();
+        String s = list.get(0);
+        System.out.println(s);
+    }
 
 }
